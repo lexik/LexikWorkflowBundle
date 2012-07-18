40d74f9ee79b8a46368d0cca4a617b0faa51e74d
Gilles Gauthier <g.gauthier@lexikteam.com>
2012-07-18 17:08:15 +0200

    Add __toString method in ValidationException

--- Exception/ValidationException.php
+++ Exception/ValidationException.php
@@ -4,5 +4,11 @@ namespace FreeAgent\WorkflowBundle\Exception;
 
 class ValidationException extends \Exception
 {
-
+     /**
+      * (non-PHPdoc)
+      * @see Exception::__toString()
+      */
+     public function __toString() {
+         return $this->getMessage();
+     }
 }
